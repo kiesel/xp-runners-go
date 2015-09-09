82c8e8fe5d92622959674963c6f9779ef94c5d4c
Alex D'Andrea <alex@dandrea.io>
2015-09-09 18:55:53 +0000

    Add tests for xp

--- xp/xp_test.go
+++ xp/xp_test.go
@@ -2,12 +2,33 @@ package main
 
 import (
   "testing"
-  "testing/quick"
+  "github.com/kiesel/xp-runners-go/runner"
 )
 
-func TestparseArgs_version(t *testing.T) {
-  c = runner.Context {}
+func expectTool(c runner.Context, actual string, t *testing.T) {
+  if (c.Tool != actual) {
+    t.Errorf("Unexpected tool '%s', was looking for '%s'", c.Tool, actual)
+  }
+}
+
+func TestParseArgs_noargs(t *testing.T) {
+  c := runner.Context {}
+  parseArgs(&c, []string {"xp.go"})
+
+  expectTool(c, "xp.runtime.ShowResource", t)
+  if (len(c.Args) != 2) {
+    t.Error("Invalid number of arguments.")
+  }
+}
+
+func TestParseArgs_version(t *testing.T) {
+  c := runner.Context {}
   parseArgs(&c, []string {"xp.go", "-v"})
+  expectTool(c, "xp.runtime.Version", t)
+}
 
-
+func TestParseArgs_evaluate(t *testing.T) {
+  c := runner.Context {}
+  parseArgs(&c, []string {"xp.go", "-e", "var_dump('Hello World');"})
+  expectTool(c, "xp.runtime.Evaluate", t)
 }
